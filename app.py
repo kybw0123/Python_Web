c65fa5919f4e69df65e6a882ce99ad62bb2eb44d
YongBae Kim <73388879+kybw0123@users.noreply.github.com>
2021-05-30 21:53:32 +0900

    회원가입 정보 받고 회원가입 버튼 누르면 json으로 돌려주는 걸로 일단 파이썬 파일 수정함 html 파일에 utf-8 적용 회원가입 입력 정보에 각각 name 지정 비어있던 form에 action, mathod에 이름 지정

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # app.py
+# -- coding: utf-8 --
 from flask import Flask, render_template, jsonify, request
 
 #Flask 객체 인스턴스 생성
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 @app.route('/') # 접속하는 url
 def index():
@@ -16,6 +18,20 @@ def board():
 @app.route("/signup")
 def signup():
     return render_template('membership.html', title='회원가입')
+
+@app.route("/user_info", methods=['POST'])
+def user_info():
+    user_id = request.form['user_id']
+    user_pw = request.form['user_pw']
+    user_pw2 = request.form['user_pw2']
+    user_name = request.form['user_name']
+    user_date = request.form['user_date']
+    user_phone = request.form['user_phone']
+    user_sms = request.form['sms']
+    user_email = request.form['user_email']
+    user_info = request.form
+
+    return user_info
 
 if __name__ =="__main__":
     app.run(debug=True)
